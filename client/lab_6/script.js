0a2cfe9af357d43c9b3d5e3fc7abf35d048af43f
kmarsh423 <marzkobie@gmail.com>
2022-03-13 17:49:39 -0400

    updated spaccing and links

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,25 +1,33 @@
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.cell(min);
+  const newMax = Math.floor(max);
+}
+
 function dataHandler(array) {
-  
+  console.log('fired dataHandler');
+  console.table(array);
+  const range = [...Array(15).keys()];
+  range.forEach((item) => {
+    console.log('range item', item);
+  });
 }
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.form');
-  form.querySelector('submit').style.display = 'none';
+  const submit = document.querySelector('.submit');
+  submit.style.display = 'none';
+
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-  if (arrayFromJson.length > 0) {
-    submit.style.display = block
+  if (arrayFromJson.data.length > 0) {
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
-      document.querySelector('.submit').style.display = 'none';
       console.log('form submission'); // this is substituting for a "breakpoint"
+      submit.style.display = 'block';
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+      dataHandler(arrayFromJson);
     });
   }
-  
-  
-  console.table(arrayFromJson); // this is called "dot notation"
-  dataHandler(arrayFromJson)
-  // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-  // it contains all 1,000 records we need
 }
 
 // this actually runs first! It's calling the function above
