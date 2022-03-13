ba93260c551849fe15935dff233bd9f7e4940601
kmarsh423 <marzkobie@gmail.com>
2022-03-13 18:06:39 -0400

    added get random int inc

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,16 +1,24 @@
 function getRandomIntInclusive(min, max) {
   const newMin = Math.cell(min);
   const newMax = Math.floor(max);
+  return Math.floor(
+    Math.random() * (newMax - newMin + 1) + newMin
+  );
 }
 
 function dataHandler(array) {
   console.log('fired dataHandler');
   console.table(array);
   const range = [...Array(15).keys()];
-  range.forEach((item) => {
-    console.log('range item', item);
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, array.length - 1);
+    console.log(listItems);
+    return array[restNum];
   });
 }
+
+
+
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.form');
   const submit = document.querySelector('.submit');
@@ -18,11 +26,12 @@ async function mainEvent() { // the async keyword means we can make API requests
 
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  console.log(arrayFromJson);
   if (arrayFromJson.data.length > 0) {
+    submit.style.display = 'block';
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
-      submit.style.display = 'block';
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
       dataHandler(arrayFromJson);
